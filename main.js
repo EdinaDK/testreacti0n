cd343ce8a62e3ebf6669f2b7b022b8e20759cc56
edinadk <edinadk@mer.ci.nsu.ru>
2022-06-02 15:41:03 +0700

    fix

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
+const btn2 = document.getElementById("btn2")
 const btn = document.getElementById("btn")
+const text = document.getElementById("reaction")
 
+function rand(){
+    return (10000-1000)*Math.random()+1000
+}
+
+
+btn2.addEventListener("click", () => {
+    setTimeout(() => {
+        //btn.style.background = 'green';
+        text.innerText = "hi";
+    }, 1000);
+});
